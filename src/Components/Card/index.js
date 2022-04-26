ebde80c2e2a9c18252f29d657e7f717884f94844
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-04-25 21:27:02 -0300

    favorites

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Style from "../Card/card.module.css";
+import Icon from '@mdi/react'
+import { mdiHeart, mdiHeartOutline } from '@mdi/js'
 import { useNavigate } from 'react-router-dom';
 
 
 function Card ({ data })  {
-  
-  const navigate = useNavigate();
+  const favRef = useRef()
+  const navigate = useNavigate(); 
+  const [isFavourite, setIsFavourite ] = useState()
 
   function handleClick(item) {
     navigate(`/${item}`)
   }
 
+  console.log(isFavourite);
+
   return data.episodes.results.map((item) => (
     <>
-      <div onClick={() => handleClick(item.id)} className={Style.content}>
+      <div
+        onClick={(event) => {
+          const preventTarget = event.target.parentElement.parentElement.id === favRef.current.parentElement.id
+          if(!preventTarget){
+            handleClick(item.id)
+          }
+        }}
+        className={Style.content}
+      >
+        <div id="teste">
+          <Icon 
+            className="favIcon"
+            ref={favRef}
+            path={isFavourite ? mdiHeart : mdiHeartOutline}
+            size={1}
+            color="#5622f4"
+            onClick={(e) => setIsFavourite(e.target.value)}
+          />
+        </div>
+        
         <span key={item.id} className={Style.data}>
           <p> Episode number:</p>
           <p>{item.episode}</p>
--- src/Components/FormSearch/index.js
+++ src/Components/FormSearch/index.js
@@ -16,7 +16,7 @@ function FormSearch({ setSearchParams, searchParams }) {
             X
           </button>
         ) : (
-         <p> <img src="../../img/lupa-para-a-escola.png" alt="" /></p>
+         <div><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAdlJREFUSEvVlUFy2kAQRX8PuJJd8A3sE1hAqMrO8g3wEnkR5wTADZITRJzAeGF5a5/AeOcqS/LcwDkC2sUVmE6NhNAEIw0uwsKzA8F/f/p3twg7PrRjfZQCvlw7BzMWfYBcgJ3MCEmAJ3VSo4ee/LWJubWAdtDyCehbBPzQi4c2yCtAO2hJAo5Sv+BLMPuPZ1Lqz50rxwHRgEFf8xuFXtSsgvwDMJwnxMrNhVcFNIhJTAB8YmAUefGgDLIELGr+nDpn1dTizoXTqH0UP8Fwswgwmf9WQ/lNTheQJ/11ndRhWSZLQO7edNS5ap0z4WLF3bROqqkFO0FzrMtVdQsTkNY+d5+LtoN2d4/maQYzFmMAxyC6DXtRt7gFybIsloDPQYu1SOjFttbVZZyGXryvf2/735sA64JcAJLQixvrnltLVNWCRtD3oRdnjbByKkO2DdGbQl7Xphu6T+qkHGubajFj0KbE6sQyaHcAGmD8CM/i79ZBK9qyWBVMGAulRuaqUEL0iXFeCJKcvcxP9PBVZmA+3GDZJWD4TOhme6scUtnzf1joHeMull8CQDIg90j5uubpKvkgJlWQrV84NsjWAF1aE8Kg08iLbvKS/xdAAam5pni2gHd83j/gL2HPDyjpxvGfAAAAAElFTkSuQmCC"/></div>
         )}
       </div>
       
